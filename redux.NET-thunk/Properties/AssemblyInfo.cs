a2d86cf3bcdf669c7e7547256b8cb2b0c9dddd1a
Ossi Väre <ossi@taiste.fi>
2015-12-18 15:12:12 +0200

    Renamed project

--- redux.NET-thunk/Properties/AssemblyInfo.cs
+++ redux.NET-thunk/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("thunk.NET")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("redux.NET-thunk")]
+[assembly: AssemblyDescription ("Thunk middleware for Redux.NET")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Taiste")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyCopyright ("Taiste Oy")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
